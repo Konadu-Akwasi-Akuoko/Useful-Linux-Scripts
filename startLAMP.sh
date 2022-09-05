166be62d94572d400a5f8cbd81631f52fe0cc757
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2022-09-05 19:22:42 +0000

    Added status to the LAMP scripts

--- startLAMP.sh
+++ startLAMP.sh
@@ -2,7 +2,10 @@
 
 echo 'Start MariaDB sever';
 sudo systemctl start mariadb.service;
+sudo systemctl status mariadb.service;
 echo '';
 
 echo 'Starting apache2'
 sudo systemctl start apache2;
+sudo systemctl status apache2;
+echo '';
