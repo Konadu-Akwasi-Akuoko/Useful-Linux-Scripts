2aa3d5d4adadfc0b700a99d26f8f7ba2b5b4cca9
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2022-09-07 12:21:43 +0000

    Added a script to run fstrim on SSD's

---
+++ fstrim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Check if fstrim is working...";
+sudo systemctl status fstrim;
+echo "";
+echo "---------------------------------------";
+echo "fstrim /";
+sudo fstrim /;
+echo "";
+echo "---------------------------------------";
+echo "fstrim -a";
+sudo fstrim -a -v;
+echo "";
+echo "---------------------------------------";
+echo "fstrim -A";
+sudo fstrim -A -v;
+
