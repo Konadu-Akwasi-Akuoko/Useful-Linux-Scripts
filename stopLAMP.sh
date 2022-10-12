080f7601c6a6997d6c1c8a261ee326da61d19312
Konadu Akwasi Akuoko <konaduakwasikuoko@gmail.com>
2022-10-12 09:28:42 +0000

    Made changes in the stopLamp.sh file

--- stopLAMP.sh
+++ stopLAMP.sh
@@ -2,11 +2,11 @@
 
 echo 'Stop MariaDB sever';
 sudo systemctl stop mariadb.service;
-sudo systemctl disable mariadb.service;
+#sudo systemctl disable mariadb.service;
 sudo systemctl status mariadb.service;
 echo '';
 
 echo 'Starting apache2'
 sudo systemctl stop apache2;
-sudo systemctl disable apache2;
+#sudo systemctl disable apache2;
 sudo systemctl status apache2;
