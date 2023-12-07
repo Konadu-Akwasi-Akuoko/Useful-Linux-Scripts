63f42158e8f0128d75a171f91be5e4e3e4c08316
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2023-12-07 15:48:10 +0000

    feat: added a greetings.sh file

---
+++ greetings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "what is your name?"
+read name
+
+echo "Hello $name, how are you doing today?"
+
+read how_are_you_doing
+
+echo "It's a good day because you said you are '$how_are_you_doing'"
+
+echo "Have a nice day"
