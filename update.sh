f5a912fff84b883d68fddb1649a4e39f9d0680d5
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-01-10 08:51:23 +0000

    feat: removed some lines from the update.sh file that is not of use

--- update.sh
+++ update.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # This script runs the apt update and upgrade commands.
-echo '';
-echo 'apt update';
+echo ''
+echo 'apt update'
 sudo apt update
 
-echo '';
-echo 'apt upgrade';
+echo ''
+echo 'apt upgrade'
 sudo apt upgrade
 
-echo '';
-echo 'Snap update';
-sudo snap refresh
+#echo '';
+#echo 'Snap update';
+#sudo snap refresh
 
-echo '';
-echo 'Flatpak update';
-sudo flatpak update
+#echo '';
+#echo 'Flatpak update';
+#sudo flatpak update
 
-echo'';
-echo'Updating Andaconda if there is an update';
+#echo'';
+#echo'Updating Andaconda if there is an update';
 #conda update conda
 #conda update anaconda
