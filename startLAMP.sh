b8c2a00ae5a21fb76693d12816088450c76a2860
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-02-16 11:20:50 +0000

    feat

--- startLAMP.sh
+++ startLAMP.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-echo 'Start MariaDB sever';
-sudo systemctl start mariadb.service;
-sudo systemctl status mariadb.service;
-echo '';
+echo 'Start MariaDB sever'
+sudo systemctl start mariadb.service
+sudo systemctl status mariadb.service
+echo ''
 
 echo 'Starting apache2'
-sudo systemctl start apache2;
-sudo systemctl status apache2;
-echo '';
+sudo systemctl start apache2
+sudo systemctl status apache2
+echo ''
